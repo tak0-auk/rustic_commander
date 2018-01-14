a7aa99b08c2d0cab360dcb7c76a2632b707c7717
takashima <tak0auk@gmail.com>
2018-01-14 15:46:38 +0900

    move rcp

--- rcp/src/main.rs
+++ rcp/src/main.rs
@@ -1,3 +1,11 @@
+const DOC: &'static str =
+"rcp
+";
+
+extern crate getopts;
+
+use getopts::Options;
+
 use std::env;
 use std::error::Error;
 use std::fs::{ File, OpenOptions };
@@ -10,13 +18,18 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    if args.len() == 1 {
+        print!("{}", DOC);
+        process::exit(0);
+    }
+
     let path = Path::new(&args[1]);
     let target = Path::new(&args[2]);
 
     let display = path.display();
 
-    if path.is_dir() {
-        println!("directory is not supported");
+    if path.is_dir() || target.is_dir() {
+        print!("directory is not supported");
         process::exit(0);
     }
 
@@ -39,6 +52,12 @@ fn main() {
         Err(expr) => panic!("{}", expr),
         Ok(_) => 0,
     };
+}
+
+fn parse_args() {
+    let mut opts = Options::new();
+
+    opts.optopt("f", "", "", "hint");
 }
 
 // fn open_file(name: &str) -> result::Result<File, io::Error> {
---
+++ rcp/target/debug/rcp.d
@@ -0,0 +1,1 @@
+D:\Rust\rustic_commander\rcp\target\debug\rcp.exe: D:\Rust\rustic_commander\rcp\src\main.rs
